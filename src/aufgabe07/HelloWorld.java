45244e5e11def8337a4a655b712a824a791bfac4
Stephan Schwarz <sschwarz@student.tgm.ac.at>
2014-03-21 10:53:09 +0100

    V2

--- src/aufgabe07/HelloWorld.java
+++ src/aufgabe07/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 	public static void helloWorld1(){
 		System.out.println("hello World");
 	}
-	
+	public static void helloWorld2(){
+		System.out.println("hello Continent");
+	}
 }
--- src/aufgabe07/Main.java
+++ src/aufgabe07/Main.java
@@ -3,6 +3,7 @@ package aufgabe07;
 public class Main {
 	public static void main(String[] args){
 		HelloWorld.helloWorld1();
+		HelloWorld.helloWorld1();
 		
 	}
 }
